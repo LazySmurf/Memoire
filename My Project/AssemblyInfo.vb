3f76490b7f09a9127bda11a1945b8d8c38883165
LazySmurf <service@lazysmurf.net>
2022-06-12 20:31:59 -0400

    Rebuilt as Release
    Rebuilt the application in the Release channel in Visual Studio. Now you're able to find the application in both the Debug and Release channels, making it easier to modify or work with the code.
    
    Also included the .bat files for launching the two alternate versions of the application in the Debug channel and their compiled executables in the Release channel.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("22.06.9.422")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("22.06.12.1957")>
+<Assembly: AssemblyFileVersion("1.0.0.1")>
 <Assembly: NeutralResourcesLanguage("en-CA")>
---
+++ bin/Debug/Memoire (Debugging Mode).bat
@@ -0,0 +1,1 @@
+start "" "%~dp0Memoire.exe" -debug
